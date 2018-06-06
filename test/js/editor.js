a51b028dae3366748aec229ebbbbea7694b3bb15
SebastiaanYN <34006210+SebastiaanYN@users.noreply.github.com>
2018-06-06 20:44:24 +0200

    Add files via upload

--- test/js/editor.js
+++ test/js/editor.js
@@ -1,9 +1,25 @@
 const editor = ace.edit("editor");
-editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/material");
 editor.session.setMode("ace/mode/javascript");
 
 editor.setValue(`function foo() {
-console.log('bar');
+    console.log('You ran the foo function!');
+    return 'bar';
 }
-foo();
+
+const number = 5;
+if (!number) throw new Error('Number must be provided!');
+console.error(\`\${number} is a number!\`);
+
+const info = {
+    infoStuff: 'correct',
+    infoStufferino: {
+        infoStuff: 'correct'
+    }
+}
+
+async function test(args, moreArguments) {
+    const response = await doAsyncStuff(arguments, moreArguments);
+    response(number).then(resp => console.log(resp)).catch(console.error);
+}
 `, -1)
