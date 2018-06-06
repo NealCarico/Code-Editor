59b23d36a9c6f749662913a1a02906ccfde82bac
SebastiaanYN <34006210+SebastiaanYN@users.noreply.github.com>
2018-06-06 16:41:01 +0200

    Updated layout
    Editor and console on the left side, and tutorial on the right side

--- test/js/editor.js
+++ test/js/editor.js
@@ -1,3 +1,9 @@
 const editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/javascript");
+
+editor.setValue(`function foo() {
+console.log('bar');
+}
+foo();
+`, -1)
